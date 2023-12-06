f25ca0680dce5e05151afbfe9a9f2ba19e7b50c9
YUREEEH <yure1921@outlook.com>
2023-12-06 15:12:23 -0300

    prompt e parseInt

--- dia1/dia1.js
+++ dia1/dia1.js
@@ -1,1 +1,4 @@
-console.log("Beba água")
+let anoNascimento = prompt("Insira seu ano de nascimento");
+let entradaNumerica = parseInt (anoNascimento)
+let idadeDoUsuario = 2023 - anoNascimento
+console.log("O usuário tem " + idadeDoUsuario + " anos.")
