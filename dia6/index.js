29f85abd0f46af189c4ff0c9086faa6da33691b0
YUREEEH <yure1921@outlook.com>
2023-12-07 17:32:11 -0300

    calculadora com FOR

---
+++ dia6/index.js
@@ -0,0 +1,5 @@
+let multiplicante = Number(prompt("Digite o numero que precisa da taboada"))
+
+for (let multiplicador = 0; multiplicador <= 10; multiplicador++){
+    console.log(multiplicante + " x " + multiplicador + " = " + (multiplicante*multiplicador))
+}
