44bdb62f2a418744e20658e8ee7c3ef8c36f6e7a
YUREEEH <yure1921@outlook.com>
2023-12-08 15:55:29 -0300

    dowhile com if simples

---
+++ dia7/exercicio1-dia7/index.js
@@ -0,0 +1,11 @@
+let agua = 0
+do {
+    let bebeu =prompt("Você bebeu água hoje? (sim/não)")
+    if(bebeu = "sim"){
+        console.log("Você tomou +1 litro de água!")
+       }
+    agua++
+} while (agua<4)
+
+
+console.log("Você cumpriu sua meta diária de hidratação, parabéns!") 
