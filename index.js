2a98f96c12ac83563b789350819c2b05354de9f0
Yuri Matheus <yure1921@outlook.com>
2024-05-08 07:18:03 -0300

    att

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('relembrando git')
+console.log(prompt()) //entrada de dados para printar na tela
+
+let entradaUsuario = prompt(); //entrada de dados salva na variavel de texto
+console.log(entradaUsuario); //printar variavel de texto na tela
+
+let variavelNumericaInt = parseInt(prompt()); //transformar a variavel em numero inteiro
+variavelNumericaInt++;
+console.log(variavelNumericaInt);
